9afa98886e51172bc9ffb2b2b3d3921f3f137672
gulludalkiran <81182319+gulludalkiran@users.noreply.github.com>
2022-09-28 00:01:56 +0300

    first commit. completed examples

---
+++ switchDemo/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        char grade = 'A';
+
+        switch (grade) {
+            case 'A':
+                System.out.println("Mükemmel : geçtiniz");
+                break;
+
+            case 'B':
+            case 'C':
+                System.out.println("İyi : geçtiniz");
+                break;
+
+            case 'D':
+                System.out.println("Fena değil : geçtiniz");
+                break;
+
+            case 'F':
+                System.out.println("Maalesef kaldınız");
+                break;
+
+            default:
+                System.out.println("Geçersiz not girdiniz");
+        }
+    }
+}
